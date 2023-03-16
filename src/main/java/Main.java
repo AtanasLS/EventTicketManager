29cb0b1bf923b749c3a4ab569888b914025e5223
AtanasLS <atanaslstoyanov.03@gmail.com>
2023-03-16 16:35:46 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+
+
+import gui.controller.LoginPageController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/gui/view/LoginPageView.fxml"));
+        primaryStage.setTitle("EventTicketManager - Please Log In");
+        // primaryStage.setFullScreen(true);
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}

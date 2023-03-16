cb2fa77250659bf58dc4deaaf1fc72e62dc98049
AtanasLS <atanaslstoyanov.03@gmail.com>
2023-03-16 18:05:38 +0100

    Adding some additional classes to the project

--- src/main/java/gui/model/LoginPageModel.java
+++ src/main/java/gui/model/LoginPageModel.java
@@ -1,4 +1,13 @@
 package gui.model;
 
-public class LoginPageModel {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginPageModel implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }

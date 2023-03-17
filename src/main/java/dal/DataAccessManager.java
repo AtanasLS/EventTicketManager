8a0c5a1bce47e315d470afae702d832e78988853
AtanasLS <atanaslstoyanov.03@gmail.com>
2023-03-17 13:09:17 +0100

    Databaseconnection - done

--- src/main/java/dal/DataAccessManager.java
+++ src/main/java/dal/DataAccessManager.java
@@ -1,5 +1,33 @@
 package dal;
 
-public class DataAccessManager {
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DataAccessManager {
+       private SQLServerDataSource dataSource;
+
+       public DataAccessManager(){
+           dataSource = new SQLServerDataSource();
+           dataSource.setServerName("10.176.111.34");
+           dataSource.setDatabaseName("CSe2022B_Event_Ticket_Manager");
+           dataSource.setUser("CSe2022B_e_7");
+           dataSource.setPassword("CSe2022BE7#");
+           dataSource.setPortNumber(1433);
+           dataSource.setTrustServerCertificate(true);
+
+       }
+       public Connection getConnection() throws SQLServerException {
+           return dataSource.getConnection();
+       }
+       //checks if connection is closed or not
+       public static void main(String[] args) throws SQLException {
+           DataAccessManager dataAccessManager = new DataAccessManager();
+           Connection connection = dataAccessManager.getConnection();
+
+           System.out.println("Is it open? " + !connection.isClosed());
+           connection.close();
+       }
 }

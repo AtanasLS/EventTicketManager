b73c0f89ee48566d30225b6a78133ef7bd07b1e9
AtanasLS <atanaslstoyanov.03@gmail.com>
2023-03-22 12:36:12 +0100

    adding the first dao class and a user object class also fixing some wierd error with fxml files.

---
+++ src/main/java/dal/dao/UserDAO.java
@@ -0,0 +1,80 @@
+package dal.dao;
+
+import be.User;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import dal.DataAccessManager;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.*;
+
+public class UserDAO {
+
+    public static ObservableList<User> getAllUsers() {
+        //creating the connector
+        DataAccessManager dataAccessManager = new DataAccessManager();
+        //opening connection
+        try (Connection connection = dataAccessManager.getConnection()) {
+            ObservableList<User> allUsers = FXCollections.observableArrayList();
+
+            String sqlGetUsers = "SELECT * FROM event_users;";
+
+            Statement statement = connection.createStatement();
+
+            //Executing sql statement to get all movies
+            if (statement.execute(sqlGetUsers)) {
+                //Saving result set
+                ResultSet resultSet = statement.getResultSet();
+
+                //Saving result data as variables
+                while (resultSet.next()) {
+                    int id = resultSet.getInt("ID");
+                    String username = resultSet.getString("Username");
+                    String password = resultSet.getString("Password");
+                    String type = resultSet.getString("Type");
+
+                    //Creating movie object
+                    User user = new User(id, username, password, type);
+
+                    //Adding movie object to list of all movies
+                    allUsers.add(user);
+                }
+            }
+            return allUsers;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void addNewUser(User newUser) throws SQLException{
+        //Creating dbConnector instance
+        DataAccessManager dataAccessManager = new DataAccessManager();
+        try(Connection connection = dataAccessManager.getConnection()) {
+            int userId = newUser.getId();
+            String userName = newUser.getUsername();
+            String password = newUser.getPassword();
+            String type = newUser.getType();
+            String sql = "INSERT INTO event_user VALUES" + "("+userId+",'"+userName+"', "+password+", '"+type+"')";
+            Statement statement = connection.createStatement();
+            if(statement.execute(sql)){
+                ResultSet resultSet = statement.getResultSet();
+                System.out.println("Inserted correctly");
+            }
+        }
+    }
+
+    public static void removeUser(String index) throws SQLException {
+        //Creating dbConnector instance
+        DataAccessManager dataAccessManager = new DataAccessManager();
+        try (Connection connection = dataAccessManager.getConnection()) {
+            String sql = "Delete FROM event_user where userName='" + index + "';";
+            System.out.println(sql);
+            Statement statement = connection.createStatement();
+            if (statement.execute(sql)) {
+                ResultSet resultSet = statement.getResultSet();
+                System.out.println("Removed correctly");
+            }
+        }
+
+    }
+}
+
--- src/main/java/gui/controller/CreateEventController.java
+++ src/main/java/gui/controller/CreateEventController.java
@@ -5,7 +5,7 @@ import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class GenerateTicketController implements Initializable {
+public class CreateEventController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {

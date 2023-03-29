a301613e341396db0d5c8fca3a966fcffea62481
AtanasLS <atanaslstoyanov.03@gmail.com>
2023-03-29 22:49:19 +0200

    Adding the generate new ticket logic

---
+++ src/main/java/gui/model/AddNewTicketModel.java
@@ -0,0 +1,53 @@
+package gui.model;
+
+import be.Customer;
+import be.Event;
+import be.Ticket;
+import be.User;
+import dal.dao.CustomerDAO;
+import dal.dao.EventDAO;
+import dal.dao.TicketDAO;
+import javafx.collections.ObservableList;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class AddNewTicketModel {
+
+    TicketDAO ticketDAO = new TicketDAO();
+    CustomerDAO customerDAO = new CustomerDAO();
+    EventDAO eventDAO = new EventDAO();
+
+    public AddNewTicketModel(){
+
+    }
+    public ObservableList<Ticket> getAllTickets(){
+        return ticketDAO.getAllTickets();
+    }
+
+    public ObservableList<Customer> getAllCustomers(){
+      return   customerDAO.getAllCustomers();
+    }
+    public ObservableList<Event> getAllEvents(){
+        return eventDAO.getAllEvents();
+    }
+    public boolean addTicket(int customerId, int eventId) throws SQLException {
+        List<Ticket> tickets = getAllTickets();
+        boolean token = false;
+
+
+        if (!token) {
+            if (tickets.size() != 0) {
+                Ticket ticket = new Ticket(tickets
+                        .get(tickets.size() - 1).getTicketId() + 1, customerId, eventId);
+                this.ticketDAO.addNewTicket(ticket);
+            }else {
+                Ticket ticket = new Ticket(1, customerId, eventId);
+                this.ticketDAO.addNewTicket(ticket);
+            }
+            return token;
+        } else {
+            return token;
+        }
+    }
+}
